87c707bccf9c9febd63faee3aa94b3081951a78d
kirby@puppetlabs.com <kirby@puppetlabs.com>
2015-05-13 14:42:01 -0700

    Rip out pynagios references

--- lib_zpr.py
+++ lib_zpr.py
@@ -6,58 +6,17 @@ This check is designed to check a file printed by the zpr_proxy user
 for the output of tsp, which tracks rsync backup jobs.
 """
 
-import pynagios
 import re
 import os
 import datetime
 
 from subprocess import check_output
-from pynagios import Response
 
 
 check_tsp_output = []
 check_tsp_job_out = []
 check_job_changes = []
 json_output = []
-
-def check_zpr_rsync_job(backup_host):
-    tspfile = '/var/lib/zpr/zpr_proxy_tsp.out'
-    tspout = []
-    check_host = []
-    for i in open(tspfile):
-        tspout.append(i.strip())
-    for i in reversed(tspout):
-        if re.compile(backup_host).findall(i):
-            check_host.append(i)
-            break
-
-    if not check_host:
-        return Response(pynagios.UNKNOWN, 'Cannot find host matching {h}'.format(h=backup_host))
-
-    c_exit = str(check_host[0]).split()[3]
-
-    if c_exit == '0':
-        return Response(pynagios.OK,
-                        "Most recent zpr backup job for %s has run successfully." % backup_host)
-    elif c_exit == '23':
-        e_23 = [
-            "Last zpr backup job for {e} has returned exit code 23.".format(e=c_exit),
-            'This is usually because of an IO error due to permission being denied',
-            'to read a file, or because of a file being deleted after the job started.',
-            'You may consider adding ignore-errors to the job.',
-        ]
-        return Response(pynagios.WARNING, ' '.join(e_23))
-    elif c_exit == '12':
-        e_12 = [
-            str("Last zpr backup job for {host}".format(host=backup_host)),
-            'has failed with exit code 12',
-            'because the ssh key for zpr_user was denied.',
-        ]
-        return Response(pynagios.CRITICAL, ' '.join(e_12))
-    elif c_exit != '0':
-        return Response(pynagios.CRITICAL,
-                        "Last zpr backup job for %s has failed with exit code %s." % (
-                            backup_host, c_exit))
 
 def check_tsp_out(
         host,
@@ -153,19 +112,6 @@ def check_tsp_job(
                     if len(check_job_changes) >= check_tsp_job_out.index(i):
                         print('\n'.join(check_job_changes[check_tsp_job_out.index(i)]))
 
-def check_zpr_rsync_nagios(jobname):
-    check_tsp_job(jobname)
-    global json_output
-    if json_output:
-        if json_output[0].get('exit_code') == '0':
-            result = Response(pynagios.OK, '{j} completed successfully'.format(j=jobname))
-        else:
-            result = Response(pynagios.CRITICAL, '{j} failed'.format(j=jobname))
-        json_output[0]['nagios_return'] = result
-    else:
-        result = Response(pynagios.UNKNOWN, 'Cannot find job {j}'.format(j=jobname))
-        json_output.append({'nagios_return': result})
-
 if __name__ == "__main__":
     # Instantiate the plugin, check it, and then exit
-    check_zpr_rsync_job(__name__)
+    check_tsp_job(__name__)
