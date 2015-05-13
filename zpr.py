e95c9eb866e4c58326329135587528d63b255a9f
kirby@puppetlabs.com <kirby@puppetlabs.com>
2015-05-13 13:53:44 -0700

    Better handle errors where no data is provided

--- zpr.py
+++ zpr.py
@@ -1,10 +1,10 @@
 #!bin/python
 import json
 import lib_zpr
-#import logging
-#from logging.handlers import RotatingFileHandler
+import logging
+from logging.handlers import RotatingFileHandler
 
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, abort
 
 
 app = Flask(__name__)
@@ -21,43 +21,62 @@ app = Flask(__name__)
 # log.setLevel(logging.DEBUG)
 # app.logger.addHandler(handler)
 
+
+
 api_version = 'v1.0'
 api_base = str('/zpr/{v}'.format(v=api_version))
 
 @app.errorhandler(404)
 def not_found(error):
+    error_name = error.__name__ if error else "Unknown-Error"
+    app.logger.warning('Request resulted in {e}'.format(e=error_name), exc_info=error)
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('{a}/job/<backup_host>/last'.format(a=api_base), methods=['GET'])
 def check_last_zpr_job(backup_host):
     lib_zpr.check_tsp_job(backup_host)
-    return jsonify({'job_result': lib_zpr.json_output})
+    if lib_zpr.json_output:
+        return jsonify({'job_result': lib_zpr.json_output})
+    else:
+        abort(404)
 
 @app.route('{a}/job/<backup_host>/last/<int:count>'.format(a=api_base), methods=['GET'])
 def check_last_count_zpr_job(backup_host, count):
     lib_zpr.check_tsp_job(backup_host, count)
-    return jsonify({'job_result': lib_zpr.json_output})
+    if lib_zpr.json_output:
+        return jsonify({'job_result': lib_zpr.json_output})
+    else:
+        abort(404)
 
 @app.route('{a}/job/<backup_host>/output'.format(a=api_base), methods=['GET'])
 def check_zpr_job_summary(backup_host):
     lib_zpr.check_tsp_job(backup_host, show_changes=True)
-    return jsonify({'job_result': lib_zpr.json_output})
+    if lib_zpr.json_output:
+        return jsonify({'job_result': lib_zpr.json_output})
+    else:
+        abort(404)
 
 @app.route('{a}/job/<backup_host>/output/<int:count>'.format(a=api_base), methods=['GET'])
 def check_zpr_job_summary_count(backup_host, count):
     lib_zpr.check_tsp_job(backup_host, count, show_changes=True)
-    return jsonify({'job_result': lib_zpr.json_output})
+    if lib_zpr.json_output:
+        return jsonify({'job_result': lib_zpr.json_output})
+    else:
+        abort(404)
 
 @app.route('{a}/job/<backup_host>/last/nagios'.format(a=api_base), methods=['GET'])
 def check_zpr_job_last_nagios(backup_host):
     lib_zpr.check_zpr_rsync_nagios(backup_host)
-    result = [
-        {
-            'nagios_return': str(lib_zpr.json_output[0].get('nagios_return')),
-            'exit_code': lib_zpr.json_output[0].get('exit_code')
-        }
-    ]
-    return jsonify({'job_result': result})
+    if lib_zpr.json_output:
+        result = [
+            {
+                'nagios_return': str(lib_zpr.json_output[0].get('nagios_return')),
+                'exit_code': lib_zpr.json_output[0].get('exit_code')
+            }
+        ]
+        return jsonify({'job_result': result})
+    else:
+        abort(404)
 
 if __name__ == '__main__':
 #   formatter = logging.Formatter(\
